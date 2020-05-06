73b223b93da31cb583370a52df9f6b1b7da582b1
MathisCertenais <mathiscertenais@gmail.com>
2020-05-06 14:30:34 +0200

    master

--- sous_sol.java
+++ sous_sol.java
@@ -1,2 +1,10 @@
 //Bonjour je suis un test 2
-int i = 1;
+Scanner sc = new Scanner(System.in);
+println("Bonjour et bienvenue dans notre maison, SIMSHouse.");
+println("Pour ouvrir la porte d'entrée, pressez 8 : ");
+int i = sc.nextInt();
+while (i != 8){
+    println("Pour ouvrir la porte d'entrée, pressez 8 : ");
+    int i = sc.nextInt();
+}
+println("Vous êtes actuellement derriere la porte d'entrée");
