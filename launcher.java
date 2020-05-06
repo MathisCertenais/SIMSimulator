aca1d8f738a9fad47f06992b8c8197afdc730758
christophe peroumal <christophe.peroumal35@gmail.com>
2020-05-06 15:44:15 +0200

    changement trois classes

--- launcher.java
+++ launcher.java
@@ -2,7 +2,7 @@
 public class launcher {
 
 public static void main(String[] args) {
-    System.out.println("Bienvenue dans notre Maison" );
+    System.out.println("Bienvenue dans notre Maison");
 }
 
 }
