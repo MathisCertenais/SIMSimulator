4833c3aa3400b7f8aa6fdff3252c158a9b2933de
christophe peroumal <christophe.peroumal35@gmail.com>
2020-05-06 17:22:23 +0200

    utilisation methode ajouter pièce

--- Initialisation.java
+++ Initialisation.java
@@ -6,29 +6,29 @@ public class Initialisation {
 public static Maison initialiser() {
     //objets de la maison
     LinkedList<String> actions = new LinkedList<String>() ;
-    Objet camera = new Objet("Caméra de surveillance","non_utilisé",actions);
+    Objet camera ;
 
     Objet lecteur_empreinte;
     Objet sonnette;
 
     Piece sejour;
     Piece exterieurAccueil;
+    // pièce séjour
+    LinkedList<Piece> piece_Ajacent_sejour= new LinkedList<Piece>();
+    ArrayList<Objet> objets_exterieur_sejour = new ArrayList<Objet>();
+    //objets_exterieur_sejour.add(camera);
+    sejour = new Sejour("Sejour",piece_Ajacent_sejour,objets_exterieur_sejour);
 
     // pièce extérieure
     LinkedList<Piece> piece_Ajacent_exterieurAccueil = new LinkedList<Piece>();
     piece_Ajacent_exterieurAccueil.add(sejour);
     ArrayList<Objet> objets_exterieurAccueil = new ArrayList<Objet>();
-    objets_exterieurAccueil.add(camera);
-    objets_exterieurAccueil.add(lecteur_empreinte);
-    objets_exterieurAccueil.add(sonnette);
+    //objets_exterieurAccueil.add(camera);
+    //objets_exterieurAccueil.add(lecteur_empreinte);
+    //objets_exterieurAccueil.add(sonnette);
     exterieurAccueil = new Exterieur("Exterieur",piece_Ajacent_exterieurAccueil,objets_exterieurAccueil);
-
-    // pièce séjour
-    LinkedList<Piece> piece_Ajacent_sejour= new LinkedList<Piece>();
-    piece_Ajacent_sejour.add(exterieurAccueil);
-    ArrayList<Objet> objets_exterieur_sejour = new ArrayList<Objet>();
-    objets_exterieur_sejour.add(camera);
-    sejour = new Sejour("Sejour",piece_Ajacent_sejour,objets_exterieur_sejour);
+    exterieurAccueil.ajouterPiece_Adj(exterieurAccueil);
+    
 
      //liste pièce rez_de_chaussee
      LinkedList<Piece> pieces_rdc = new LinkedList<Piece>();
