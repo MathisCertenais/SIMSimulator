4e992a778bb7a4208c32bea90720771071da3cad
MathisCertenais <mathiscertenais@gmail.com>
2020-05-06 16:20:44 +0200

    master

--- Maison.java
+++ Maison.java
@@ -2,13 +2,13 @@ import java.util.LinkedList;
 
 public class Maison {
 
-    private LinkedList Liste_etages = new LinkedList<Etage>();
-    Liste_etages.add(Etage sous_sol);
-    Liste_etages.add(Etage rez_de_chaussee);
-    Liste_etages.add(Etage premier_etage);
-    Liste_etages.add(Etage toit);
-    public Maison(LinkedList Liste_etages) {
-        this.Liste_etages = Liste_etages;
+    private LinkedList<Etage> liste_etages = new LinkedList<Etage>();
+    //Liste_etages.add(sous_sol);
+    //Liste_etages.add(rez_de_chaussee);
+    //Liste_etages.add(premier_etage);
+    //Liste_etages.add(toit);
+    public Maison(LinkedList<Etage> liste_etages) {
+        this.liste_etages = liste_etages;
 
     }
 
