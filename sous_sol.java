a23e7c551e98ecd5843e3c5359de94822aaf5d9d
christophe peroumal <christophe.peroumal35@gmail.com>
2020-05-06 14:45:59 +0200

    clean code

--- sous_sol.java
+++ sous_sol.java
@@ -1,9 +1,16 @@
-Scanner sc = new Scanner(System.in);
-println("Bonjour et bienvenue dans notre maison, SIMSHouse.");
-println("Pour ouvrir la porte d'entrée, pressez 8 : ");
-int i = sc.nextInt();
-while (i != 8){
-    println("Pour ouvrir la porte d'entrée, pressez 8 : ");
-    int i = sc.nextInt();
+import java.util.Scanner;
+
+public class sous_sol{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bonjour et bienvenue dans notre maison, SIMSHouse.");
+        System.out.println("Pour ouvrir la porte d'entrée, pressez 8 : ");
+        int i = sc.nextInt();;
+    while (i != 8){
+        System.out.println("Pour ouvrir la porte d'entrée, pressez 8 : ");
+         
+    }
+    System.out.println("Vous êtes actuellement derriere la porte d'entrée");
+    
+    }
 }
-println("Vous êtes actuellement derriere la porte d'entrée");
