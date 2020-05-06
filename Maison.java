4e6a3f9f8f27e5e9c0ab6479b507cf148dea16fc
MathisCertenais <mathiscertenais@gmail.com>
2020-05-06 16:47:54 +0200

    master

--- Maison.java
+++ Maison.java
@@ -3,14 +3,26 @@ import java.util.LinkedList;
 public class Maison {
 
     private LinkedList<Etage> liste_etages = new LinkedList<Etage>();
+    private Etage memory_etage;
+    private Piece memory_piece;
     //Liste_etages.add(sous_sol);
     //Liste_etages.add(rez_de_chaussee);
     //Liste_etages.add(premier_etage);
     //Liste_etages.add(toit);
+
     public Maison(LinkedList<Etage> liste_etages) {
         this.liste_etages = liste_etages;
 
     }
 
+    public void setEtage(Etage etage_actuel){
+        //verifier si l'etage est bien présent dans la maison
+        memory_etage = etage_actuel;
+    }
 
+
+    public void setPiece(Piece piece_actuel){
+        //verifier si l'etage est bien présent dans la maison
+        memory_piece = piece_actuel;
+    }
 }
