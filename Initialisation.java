32de2abce2d846be9b93a408862686139bc69ba6
christophe peroumal <christophe.peroumal35@gmail.com>
2020-05-06 16:39:36 +0200

    add d'un e to exterieur et ajour initialisation

--- Initialisation.java
+++ Initialisation.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 
- public class Initialisation {
-Maison maMaison = new Maison();
-    
+public class Initialisation {
+
+public Maison initialiser() {
+    //Liste étage 
+    Etage rez_de_chaussee = new Etage() {
+    };
+    LinkedList<Piece> liste_etages
+
+    Maison maMaison = new Maison(liste_etages)
+   
+    Objet camera ;
+    Objet lecteur_empreinte;
+    Objet sonnette;
+
+    Piece sejour;
+
+    // pièce extérieure
+    LinkedList<Piece> piece_Ajacent_exterieurAccueil = new LinkedList<Piece>();
+    piece_Ajacent_exterieurAccueil.add(sejour);
+    Piece exterieureAccueil = new Exterieure(piece_Ajacent, new ArrayList<>().add())
+
+    // pièce séjour
+    LinkedList<Piece> piece_Ajacent_sejour= new LinkedList<Piece>();
+    piece_Ajacent_sejour.add(sejour);
+    Piece sejour = new Sejour(piece_Ajacent, list_objet)
+
+    return maMaison;
+}
+
+
+
 }
