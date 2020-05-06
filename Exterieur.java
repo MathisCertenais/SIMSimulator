41f49ef401f3bee550838096d13449bd8d4b6216
christophe peroumal <christophe.peroumal35@gmail.com>
2020-05-06 16:22:28 +0200

    ajout code extérieur & modif code piece

--- Exterieur.java
+++ Exterieur.java
@@ -1,12 +1,10 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Exterieur extends Piece {
     
-    public Exterieur() {
-        super();
+    public Exterieur(LinkedList<Piece> Piece_Ajacent,ArrayList<Objet> list_objet) {
+        super(Piece_Ajacent,list_objet);
     }
-
-
-public static void main(String[] args) {
-    
-}
 
 }
--- Piece.java
+++ Piece.java
@@ -3,11 +3,17 @@ import java.util.LinkedList;
 
 public abstract class Piece {
     // Attributs de la Pièce
-    LinkedList<Piece> Piece_Ajacente;
+    LinkedList<Piece> piece_Ajacente;
     ArrayList<Objet> list_objets;  
 
-    public Piece(LinkedList<Piece> Piece_Ajacent,ArrayList<Objet> list_objet) {
-        this.Piece_Ajacente =  Piece_Ajacent;
+    public Piece(LinkedList<Piece> piece_Ajacent,ArrayList<Objet> list_objet) {
+        this.piece_Ajacente =  piece_Ajacent;
         this.list_objets =  list_objet;
+    }
+    public LinkedList<Piece> Getpiece_Ajacent(){
+        return  this.piece_Ajacente;
+    }
+    public ArrayList<Objet> Getlist_objet(){
+        return  this.list_objets;
     }
 }
