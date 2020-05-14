480ed01a08dfdc68b24378958a3cf452fb3b8575
Paul Gougeon <paul.gougeon.1@etudiant.univ-rennes1.fr>
2020-05-14 14:38:59 +0200

    Salle sous sol

---
+++ SimsSimulatorJava/src/pieces/Garage.java
@@ -0,0 +1,16 @@
+package pieces;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import objets.Objet;
+
+public class Garage extends Piece {
+
+    public Garage(String nom, LinkedList<Piece> piece_Ajacent, ArrayList<Objet> list_objet) {
+        super(nom, piece_Ajacent, list_objet);
+    }
+
+
+    
+}
