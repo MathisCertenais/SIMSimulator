4abfbd2c57c04cb3c8df95f2672fd384efc34e5e
Maxime <maximegerbaud@orange.fr>
2020-05-14 15:02:12 +0200

    Ajout de Fonction de base sur des Objets

--- SimsSimulatorJava/src/objets/Douche.java
+++ SimsSimulatorJava/src/objets/Douche.java
@@ -6,5 +6,12 @@ public class Douche extends Objet {
     
     public Douche(String nom, String etat, LinkedList<String> actions){
        super(nom,etat,actions);
+       actions.add("Allumer l'eau");    //Ajout des Actions de Bases de la douche
+       actions.add("Eteindre l'eau");
+       actions.add("Augmenter la temperature");
+       actions.add("Diminuer la temperature");
+       actions.add("Entrer dans la douche");
+       actions.add("Sortir de la douche");
+
     }
 }
--- SimsSimulatorJava/src/objets/EnceinteConnecte.java
+++ SimsSimulatorJava/src/objets/EnceinteConnecte.java
@@ -6,5 +6,10 @@ public class EnceinteConnecte extends Objet {
     
     public EnceinteConnecte(String nom, String etat, LinkedList<String> actions){
        super(nom,etat,actions);
+       actions.add("Allumer l'enceinte");    //Ajout des Actions de Bases de l'enceinte connecté
+       actions.add("Eteindre l'Enceinte");
+       actions.add("Changer de musique");
+       actions.add("Augmenter le volume");
+       actions.add("Diminuer le volume");
     }
 }
