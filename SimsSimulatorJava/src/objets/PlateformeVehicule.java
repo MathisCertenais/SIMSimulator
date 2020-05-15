c24fad0c43512c369027c1a5723899f047427d95
Paul Gougeon <paul.gougeon.1@etudiant.univ-rennes1.fr>
2020-05-15 16:24:03 +0200

    RealiserAction de PlateformesVehicule

--- SimsSimulatorJava/src/objets/PlateformeVehicule.java
+++ SimsSimulatorJava/src/objets/PlateformeVehicule.java
@@ -1,11 +1,37 @@
 package objets;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PlateformeVehicule extends Objet {
+
+    ArrayList<String> vehicules = new ArrayList<String>();
     
     public PlateformeVehicule(){
        super("Platforme à vehicule", "operationnel");
-       actions.add("Choisir une voiture");    //Ajout des Actions de Bases de la plateforme a vehicule
        actions.add("Utiliser la plateforme a vehicule");
+       vehicules.add("Voiturette de golf");
+       vehicules.add("Renault Scenic");
+       vehicules.add("Audi TT");
+       vehicules.add("Tesla Roadster Edition Space X");
+    }
+
+    @Override
+    public void realiserAction(int c) {
+        if(c == 0){
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Liste des vehicules :");
+            for(int i=0; i<vehicules.size(); i++){
+                System.out.println(i + "- " + vehicules.get(i));
+            }
+            System.out.print("Selectionner un vehicule : ");
+            int idVehicule = scan.nextInt();
+            System.out.println("L'ascenseur s'active et vous pouvez admirer votre magnifique " 
+                + vehicules.get(idVehicule));
+        }
+        else {
+            System.out.println("Il ne se passe rien"); 
+        }
+        
     }
 }
