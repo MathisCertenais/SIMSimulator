8a44c8569be2e7063c858777c6b23860d7ef4ae3
Paul Gougeon <paul.gougeon.1@etudiant.univ-rennes1.fr>
2020-05-15 16:40:20 +0200

    objet plaque

--- SimsSimulatorJava/src/objets/Plaque.java
+++ SimsSimulatorJava/src/objets/Plaque.java
@@ -2,12 +2,70 @@ package objets;
 
 
 public class Plaque extends Objet {
+
+    private int puissance;
     
     public Plaque(){
        super("Plaque de cuisson", "éteinte");
+       puissance = 0;
        actions.add("Allumer la plaque");    //Ajout des Actions de Bases de la plaque
        actions.add("Eteindre la plaque");
        actions.add("Augmenter la puissance de la plaque");    
        actions.add("Diminuer la puissance de la plaque");
+    }
+
+    @Override
+    public void realiserAction(int c) {
+        if(c == 0){
+            if(this.etat == "allumée"){
+                System.out.println("La plaque est déjà allumeée");
+            }
+            else {
+                this.etat = "allumée";
+                this.puissance = 0;
+                System.out.println("La plaque s'allume");
+            }
+        }
+        else if(c == 1){
+            if(this.etat == "éteinte"){
+                System.out.println("La plaque est déjà éteinte");
+            }
+            else {
+                this.etat = "éteinte";
+                System.out.println("La plaque s'éteint");
+            }
+        }
+        else if(c == 2 && this.etat == "allumée"){
+            if(puissance < 10){
+                puissance++;
+                System.out.println("Vous augmentez la puissance");
+            }
+            else{
+                System.out.println("La puissance est à son maximum");
+            }
+        }
+        else if(c == 3 && this.etat == "allumée"){
+            if(puissance > 0){
+                puissance--;
+                System.out.println("Vous diminuez la puissance");
+            }
+            else{
+                System.out.println("La puissance est à son minimum");
+            }
+        }
+        else {
+            System.out.println("Il ne se passe rien"); 
+        }
+        
+    }
+
+    @Override
+    public String toString(){
+        if(etat == "allumée"){
+            return("La plaque est allumé à la puissance " + puissance);
+        }
+        else{
+            return("La plaque est éteinte");
+        }
     }
 }
