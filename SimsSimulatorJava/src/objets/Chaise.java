51a04177ba35fd26c3980304093f64d13303d9bb
Maxime <maximegerbaud@orange.fr>
2020-05-18 16:01:02 +0200

    ajout de chaise

---
+++ SimsSimulatorJava/src/objets/Chaise.java
@@ -0,0 +1,31 @@
+package objets;
+
+public class Chaise extends Objet {
+
+    public Chaise(){
+       super("Chaise","disponible");
+       actions.add("s'assoir sur La chaise");    //Ajout des Actions de Bases pour la chaise
+       actions.add("se lever de la chaise");
+    }
+
+    @Override
+    public void realiserAction(int c) {
+        if (c == 0) {
+            if (this.etat == "occupé") {
+                System.out.println("La chaise est deja occupé");
+            } else {
+                this.etat = "occupe";
+                System.out.println("vous vous asseyez dans votre chaise");
+            }
+        } else if (c == 1) {
+            if (this.etat == "disponible") {
+                System.out.println("Pour vous levez il faudrait deja y etre assis");
+            } else {
+                this.etat = "disponible";
+                System.out.println("Vous vous levez de votre chaise");
+            }
+        } else {
+            System.out.println("Il ne se passe rien");
+        }
+    }
+}
