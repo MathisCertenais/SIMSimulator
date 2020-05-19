6ea005778bb065cce34b88a89b03f1fa8ebf1f21
Maxime <maximegerbaud@orange.fr>
2020-05-19 16:29:34 +0200

    maj cadre

--- SimsSimulatorJava/src/objets/CadreConnecte.java
+++ SimsSimulatorJava/src/objets/CadreConnecte.java
@@ -1,57 +1,48 @@
 package objets;
 
-
 public class CadreConnecte extends Objet {
-    
-    public CadreConnecte(){
-       super("Cadre Connecte","actif");            //Ajout des Actions de Bases pour le cadre connecte
-       actions.add("Eteindre le cadre");
-       actions.add("Allumer le cadre");
-       actions.add("Changer de photo");    
-       actions.add("Regarder la photo");
-       
+
+    public CadreConnecte() {
+        super("Cadre Connecte", "actif"); // Ajout des Actions de Bases pour le cadre connecte
+        actions.add("Eteindre le cadre");
+        actions.add("Allumer le cadre");
+        actions.add("Changer de photo");
+        actions.add("Regarder la photo");
+
     }
 
     @Override
     public void realiserAction(int c) {
-        if(c == 0){
-            if(this.etat == "éteint"){
+        if (c == 0) {
+            if (this.etat == "éteint") {
                 System.out.println("Le cadre connecté est déjà eteint");
-            }
-            else {
+            } else {
                 this.etat = "eteint";
                 System.out.println("Le cadre connecté s'eteint");
             }
-        }
-        else if(c == 1){
-            if(this.etat == "actif"){
+        } else if (c == 1) {
+            if (this.etat == "actif") {
                 System.out.println("Le cadre connecté est déjà actif");
-            }
-            else {
+            } else {
                 this.etat = "actif";
                 System.out.println("Le cadre connecté s'allume");
             }
-        }
-        else if(c == 2){
-            if(this.etat == "eteint"){
+        } else if (c == 2) {
+            if (this.etat == "eteint") {
                 System.out.println("Veuillez d'abord allumer le cadre avant de changer de photo");
-            }
-            else {
+            } else {
                 this.etat = "actif";
                 System.out.println("*changment de la photo*");
             }
-        }else if(c == 3){
-                if(this.etat == "eteint"){
-                    System.out.println("*Super...Vous regardez en écran noir*");
-                }
-                else {
-                    this.etat = "actif";
-                    System.out.println("*vous regardez la photo*");
-                }
+        } else if (c == 3) {
+            if (this.etat == "eteint") {
+                System.out.println("*Super...Vous regardez en écran noir*");
+            } else {
+                this.etat = "actif";
+                System.out.println("*vous regardez la photo*");
             }
-             else {
-            System.out.println("Il ne se passe rien"); 
-            }
+        } else {
+            System.out.println("Il ne se passe rien");
         }
     }
 }
