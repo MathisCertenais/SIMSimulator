ddfcbba8374133932985533b943112a05e7792d7
christophe peroumal <christophe.peroumal35@gmail.com>
2020-05-20 17:12:17 +0200

    ajout d'un commentaire cocasse

--- SimsSimulatorJava/src/launcher.java
+++ SimsSimulatorJava/src/launcher.java
@@ -230,7 +230,7 @@ public class launcher {
                 System.out.print("Tapez le numéro correspondant : ");
                 int idObjetAjout = scan.nextInt();
 
-                //Ajout de l'objet
+                //Ajout de l'objet nksm c'est de la merde
                 boolean present = false;
                 for(int i=0; i<maison.getPiece().getlist_objet().size(); i++){    
                    present = maison.getPiece().getlist_objet().get(i).getNom()
