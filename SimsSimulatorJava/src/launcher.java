8c55c54990b179c9a0b02cb3e0af2159ecef470c
Paul Gougeon <paul.gougeon.1@etudiant.univ-rennes1.fr>
2020-05-20 15:48:27 +0200

    admin peut changer l'heure

--- SimsSimulatorJava/src/launcher.java
+++ SimsSimulatorJava/src/launcher.java
@@ -9,124 +9,148 @@ import pieces.Piece;
 
 public class launcher {
 
-    public static void boucleJeu(boolean admin, Maison maison, Scanner scan){
+    public static void boucleJeu(boolean admin, Maison maison, Scanner scan) {
         boolean play = true;
-            while (play) {
-                maison.checkJour();
-                System.out.println(maison);
+        while (play) {
+            maison.checkJour();
+            System.out.println(maison);
 
-                // Choix de la premiere action
-                System.out.println("Vous pouvez :");
-                System.out.println("0- Quitter le jeu");
-                System.out.println("1- Changer de piece");
-                System.out.println("2- Inspecter un objet");
-                if (maison.getPiece() instanceof Ascenseur) {
-                    System.out.println("3- Changer d'étage");
-                }
-                System.out.print("Tapez le chiffre correspondant à l'action : ");
-                int idAction = scan.nextInt();
+            // Choix de la premiere action
+            System.out.println("Vous pouvez :");
+            System.out.println("0- Quitter le jeu");
+            System.out.println("1- Changer de piece");
+            System.out.println("2- Inspecter un objet");
+            if (maison.getPiece() instanceof Ascenseur) {
+                System.out.println("3- Changer d'étage");
+            }
+            if (admin) {
+                System.out.println("454- Fixer l'heure");
+                System.out.println("666- Ajouter un objet dans la pièce");
+            }
+            System.out.print("Tapez le chiffre correspondant à l'action : ");
+            int idAction = scan.nextInt();
 
-                // Quitter le jeu
-                if (idAction == 0) {
-                    play = false;
-                    System.out.println("Merci d'avoir jouer !!");
-                }
+            // Quitter le jeu
+            if (idAction == 0) {
+                play = false;
+                System.out.println("Merci d'avoir jouer !!");
+            }
 
-                // Changement de piece
-                if (idAction == 1) {
+            // Changement de piece
+            if (idAction == 1) {
 
-                    // Affichage des pieces
-                    System.out.println("Aller dans la piece :");
-                    LinkedList<Piece> pieces_possibles = maison.getPiece().getpiece_Ajacent();
-                    for (int i = 0; i < pieces_possibles.size(); i++) {
-                        System.out.println(i + "- " + pieces_possibles.get(i).getNom());
-                    }
-
-                    // Choix de la piece
-                    System.out.print("Tapez le chiffre correspondant à la pièce : ");
-                    int idPiece = scan.nextInt();
-                    if (idPiece > -1 && idPiece < pieces_possibles.size()) {
-                        maison.setPiece(pieces_possibles.get(idPiece));
-                        for (Objet objet : pieces_possibles.get(idPiece).getlist_objet()) {
-                            objet.actualiser(maison.getJour());
-                        }
-                        maison.passageTps(0.3);
-                    } else {
-                        System.out.println("Erreur, le numéro ne correspond à aucune piece");
-                    }
+                // Affichage des pieces
+                System.out.println("Aller dans la piece :");
+                LinkedList<Piece> pieces_possibles = maison.getPiece().getpiece_Ajacent();
+                for (int i = 0; i < pieces_possibles.size(); i++) {
+                    System.out.println(i + "- " + pieces_possibles.get(i).getNom());
                 }
 
-                // Inspection d'un objet
-                else if (idAction == 2) {
-
-                    // Affichage des objets
-                    System.out.println("Inspecter quel objet :");
-                    ArrayList<Objet> objets_dispo = maison.getPiece().getlist_objet();
-                    for (int i = 0; i < objets_dispo.size(); i++) {
-                        System.out.println(i + "- " + objets_dispo.get(i).getNom());
+                // Choix de la piece
+                System.out.print("Tapez le chiffre correspondant à la pièce : ");
+                int idPiece = scan.nextInt();
+                if (idPiece > -1 && idPiece < pieces_possibles.size()) {
+                    maison.setPiece(pieces_possibles.get(idPiece));
+                    for (Objet objet : pieces_possibles.get(idPiece).getlist_objet()) {
+                        objet.actualiser(maison.getJour());
                     }
-
-                    // Choix de l'objet
-                    System.out.print("Tapez le chiffre correspondant à l'objet : ");
-                    int idObjet = scan.nextInt();
-                    if (idObjet > -1 && idObjet < objets_dispo.size()) {
-
-                        // Focus sur un objet
-                        boolean focusObj = true;
-                        while (focusObj) {
-                            System.out.println(objets_dispo.get(idObjet));
-
-                            // Affichage des actions
-                            System.out.println("Actions réalisable :");
-                            LinkedList<String> actions_dispo = objets_dispo.get(idObjet).getAction();
-                            for (int i = 0; i < actions_dispo.size(); i++) {
-                                System.out.println(i + "- " + actions_dispo.get(i));
-                            }
-                            System.out.println(actions_dispo.size() + "- Laisser l'objet tranquille");
-
-                            // Choix de l'action
-                            System.out.print("Tapez le chiffre correspondant à l'objet : ");
-                            int idActionObj = scan.nextInt();
-                            if (idActionObj > -1 && idActionObj < actions_dispo.size()) {
-                                maison.getPiece().getlist_objet().get(idObjet).realiserAction(idActionObj);
-                                maison.passageTps(0.15);
-                            } else if (idActionObj == actions_dispo.size()) {
-                                focusObj = false;
-                            } else {
-                                System.out.println("Erreur, le noméro ne correspond à aucune action");
-                            }
-                        }
-                    } else {
-                        System.out.println("Erreur, le numéro ne correspond à aucun objet");
-                    }
-
-                }
-
-                // Changer d'etage
-                else if (idAction == 3 && maison.getPiece() instanceof Ascenseur) {
-
-                    // Affichage des etages
-                    System.out.println("Liste des étages :");
-                    for (int i = 0; i < maison.getAllEtage().size(); i++) {
-                        System.out.println(i + "- " + maison.getAllEtage().get(i).getNom());
-                    }
-
-                    //Choix de l'étage
-                    System.out.print("Tapez le chiffre correspondant à l'étage : ");
-                    int idEtage = scan.nextInt();
-                    if (idEtage > -1 && idEtage < maison.getAllEtage().size()) {
-                        maison.setEtage(maison.getAllEtage().get(idEtage));
-                        for (int i = 0; i < maison.getEtage().getAllPiece().size(); i++) {
-                            if (maison.getEtage().getPiece(i) instanceof Ascenseur) {
-                                maison.setPiece(maison.getEtage().getPiece(i));
-                            }
-                        }
-                        maison.passageTps(0.5);
-                    } else {
-                        System.out.println("Erreur, le numéro ne correspond à aucun étage");
-                    }
+                    maison.passageTps(0.3);
+                } else {
+                    System.out.println("Erreur, le numéro ne correspond à aucune piece");
                 }
             }
+
+            // Inspection d'un objet
+            else if (idAction == 2) {
+
+                // Affichage des objets
+                System.out.println("Inspecter quel objet :");
+                ArrayList<Objet> objets_dispo = maison.getPiece().getlist_objet();
+                for (int i = 0; i < objets_dispo.size(); i++) {
+                    System.out.println(i + "- " + objets_dispo.get(i).getNom());
+                }
+
+                // Choix de l'objet
+                System.out.print("Tapez le chiffre correspondant à l'objet : ");
+                int idObjet = scan.nextInt();
+                if (idObjet > -1 && idObjet < objets_dispo.size()) {
+
+                    // Focus sur un objet
+                    boolean focusObj = true;
+                    while (focusObj) {
+                        System.out.println(objets_dispo.get(idObjet));
+
+                        // Affichage des actions
+                        System.out.println("Actions réalisable :");
+                        LinkedList<String> actions_dispo = objets_dispo.get(idObjet).getAction();
+                        for (int i = 0; i < actions_dispo.size(); i++) {
+                            System.out.println(i + "- " + actions_dispo.get(i));
+                        }
+                        System.out.println(actions_dispo.size() + "- Laisser l'objet tranquille");
+
+                        // Choix de l'action
+                        System.out.print("Tapez le chiffre correspondant à l'objet : ");
+                        int idActionObj = scan.nextInt();
+                        if (idActionObj > -1 && idActionObj < actions_dispo.size()) {
+                            maison.getPiece().getlist_objet().get(idObjet).realiserAction(idActionObj);
+                            maison.passageTps(0.15);
+                        } else if (idActionObj == actions_dispo.size()) {
+                            focusObj = false;
+                        } else {
+                            System.out.println("Erreur, le noméro ne correspond à aucune action");
+                        }
+                    }
+                } else {
+                    System.out.println("Erreur, le numéro ne correspond à aucun objet");
+                }
+
+            }
+
+            // Changer d'etage
+            else if (idAction == 3 && maison.getPiece() instanceof Ascenseur) {
+
+                // Affichage des etages
+                System.out.println("Liste des étages :");
+                for (int i = 0; i < maison.getAllEtage().size(); i++) {
+                    System.out.println(i + "- " + maison.getAllEtage().get(i).getNom());
+                }
+
+                // Choix de l'étage
+                System.out.print("Tapez le chiffre correspondant à l'étage : ");
+                int idEtage = scan.nextInt();
+                if (idEtage > -1 && idEtage < maison.getAllEtage().size()) {
+                    maison.setEtage(maison.getAllEtage().get(idEtage));
+                    for (int i = 0; i < maison.getEtage().getAllPiece().size(); i++) {
+                        if (maison.getEtage().getPiece(i) instanceof Ascenseur) {
+                            maison.setPiece(maison.getEtage().getPiece(i));
+                        }
+                    }
+                    maison.passageTps(0.5);
+                } else {
+                    System.out.println("Erreur, le numéro ne correspond à aucun étage");
+                }
+            }
+
+            // Changement de l'heure
+            else if (idAction == 454) {
+                System.out.println("Avancez dans le temps de combien d'heures ?");
+                System.out.println("Tapez le nombre en heure avec une virgule pour les minutes");
+                System.out.print("(Exemple : 8h30 = 8,5) : ");
+                double heure = scan.nextDouble();
+                maison.passageTps(heure);
+            }
+
+            // Ajout d'un objet
+            else if (idAction == 666) {
+
+            } 
+            
+            //Mauvais numéro
+            else {
+                System.out.println("Erreur, le numéro est incorrect");
+            }
+
+        }
     }
 
     public static void main(String[] args) {
